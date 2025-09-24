a0415d2358215ce73a1c606775f560f1ea425424
Wyatt Mullen <mullenw@usf.edu>
2025-09-24 19:16:48 -0400

    first commit

---
+++ cc_5.js
@@ -0,0 +1,24 @@
+//const customer = {name: "Bob", job: "Builder", salary: 100000};
+
+//function formatData(obj){
+ //   return `${obj.name}
+
+//let userCount = 0;
+
+//function addUser (username) {
+    userCount++
+    console.log(`${userName})`)
+
+
+let nummbers = [1,2,3,4,5]
+
+function addNumbers(numList) {
+    return numList.reduce ((sum, n) => sum + n,0)
+}
+
+function averageNumbers(fun, numList){
+    return fun(numlist) / numList.length
+}
+let numAvg= averageNumbers(addNumbers, numbers);
+
+console.log(numAvg);
