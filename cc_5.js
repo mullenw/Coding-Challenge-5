cfa5dc49d495dad4c821c5d7ca22eb921af6b1f5
Wyatt Mullen <mullenw@usf.edu>
2025-09-28 10:50:44 -0400

    process payroll

--- cc_5.js
+++ cc_5.js
@@ -11,3 +11,21 @@ function calculateOvertimePay(rate, hours) {
     const overtimeHours = Math.max(hours - 40,0);
     return overtimeHours * rate * 1.5;
 }
+function calculateTaxes(grossPay) {
+    return grossPay * 0.15;
+}
+function processPayroll(employee) {
+    const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+    const overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
+    const grossPay = basePay + overtimePay;
+    const taxes = calculateTaxes(grossPay);
+    const netPay = grossPay - taxes;
+
+    return {
+        name: employee.name,
+        basepay: basePay.toFixed(2),
+        overtimePay: overtimePay.toFixed(2),
+        grossPay: grossPay.toFixed(2),
+        netPay: netPay.toFixed(2)
+    };
+}
